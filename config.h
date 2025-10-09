d18b7fe0770b74b59e96f79a6247781595a51399
Jan Zeuke <jan@zeuke.me>
2025-10-10 00:22:03 +0200

    CHANGE: config.h

--- config.h
+++ config.h
@@ -2,7 +2,7 @@
 #define CONFIG_H
 
 // String used to delimit block outputs in the status.
-#define DELIMITER "  "
+#define DELIMITER " | "
 
 // Maximum number of Unicode characters that a block can output.
 #define MAX_BLOCK_OUTPUT_LENGTH 45
@@ -18,15 +18,12 @@
 
 // Define blocks for the status feed as X(icon, cmd, interval, signal).
 #define BLOCKS(X)             \
-    X("", "sb-mail", 600, 1)  \
-    X("", "sb-music", 0, 2)   \
-    X("", "sb-disk", 1800, 3) \
     X("", "sb-memory", 10, 4) \
-    X("", "sb-loadavg", 5, 5) \
-    X("", "sb-mic", 0, 6)     \
-    X("", "sb-record", 0, 7)  \
-    X("", "sb-volume", 0, 8)  \
-    X("", "sb-battery", 5, 9) \
-    X("", "sb-date", 1, 10)
+    X("", "sb-cpu", 5, 5) \
+    X("", "sb-cpubars", 10, 6)     \
+    X("", "sb-volume", 0, 10)  \
+    X("", "sb-battery", 5, 8)  \
+    X("", "sb-internet", 5, 9) \
+    X("", "sb-clock", 60, 7)
 
 #endif  // CONFIG_H
